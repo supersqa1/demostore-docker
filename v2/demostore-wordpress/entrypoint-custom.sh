95ebe906c7c28c271a31496e53acb1b34887d723
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2024-10-26 22:26:46 -0700

    wip

--- v2/demostore-wordpress/entrypoint-custom.sh
+++ v2/demostore-wordpress/entrypoint-custom.sh
@@ -3,6 +3,13 @@
 set -x
 set -e
 
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new-site-url>"
+  exit 1
+fi
+
+NEW_URL="$1"
 
 # Run the default WordPress entrypoint (adding & so that the next steps get executed)
 /usr/local/bin/docker-entrypoint.sh apache2-foreground &
@@ -25,9 +32,17 @@ WORDPRESS_IP=$WORDPRESS_IP
 if [[ "$WORDPRESS_IP" != "0.0.0.0" ]]; then
     echo "Replacing IP address..."
     echo "New IP Address = $WORDPRESS_IP"
+    wp search-replace 127.0.0.1 $WORDPRESS_IP --all-tables --allow-root
     wp search-replace 0.0.0.0 $WORDPRESS_IP --all-tables --allow-root
     echo "Done replacing IP address"
 fi
+
+# change owner of all folders in wp-content
+chown -R www-data:www-data /var/www/html/wp-content
+
+# Check if URL argument is provided
+wp option update siteurl "$NEW_URL"
+wp option update home "$NEW_URL"
 
 
 # Keep the container running
